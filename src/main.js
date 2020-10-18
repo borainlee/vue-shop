09a4ba71da7387f8f7fa7b2945fc9f91ec5fd7f8
博宇 <741567165@qq.com>
2020-10-18 11:41:52 +0800

    项目初始化提交

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import './plugins'
+// 以下是完整引入
+// import ElementUI from 'element-ui'
+// Vue.use(ElementUI)
 Vue.config.productionTip = false
-
 new Vue({
   router,
   render: h => h(App)
---
+++ src/plugins/index.js
@@ -0,0 +1,5 @@
+import Vue from 'vue'
+import 'element-ui/lib/theme-chalk/index.css'
+import { Button, input } from 'element-ui'
+Vue.use(Button)
+Vue.use(input)
